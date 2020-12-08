19e3468541e308277550a959d773ec05594536ab
Johannes Brinksmeier <1258430+jbrinksmeier@users.noreply.github.com>
2020-12-09 00:54:19 +0100

    fix domain list creation

--- create-indicators-for-tinycheck.py
+++ create-indicators-for-tinycheck.py
@@ -4,15 +4,12 @@ DOMAINS_FILE = 'network.csv'
 CERTIFICATES_FILE = 'certificates.yaml'
 
 def load_stalkerware_domains():
-    domains = []
     try:
         with open(DOMAINS_FILE, 'r') as domains_file:
             csv_contents = csv.reader(domains_file, delimiter=',')
-            for line in csv_contents:
-                domains.append(line[1])
-            return [line[1] in line in csv_contents]
+            return [line[1] for line in csv_contents]
     except Exception:
-        print("Error loading {}: {}".format(DOMAINS_FILE, Exception.__format__()))
+        print("Error loading {}".format(DOMAINS_FILE))
         return False
 
 
@@ -20,9 +17,8 @@ def load_stalkerware_certificates():
     try:
         with open(CERTIFICATES_FILE, 'r') as cert_file:
             return yaml.load(cert_file, yaml.SafeLoader)
-
     except Exception:
-        print("Error loading {}: {}".format(DOMAINS_FILE, Exception.__format__()))
+        print("Error loading {}".format(DOMAINS_FILE))
         return False
 
 iocs = {'iocs': []}
