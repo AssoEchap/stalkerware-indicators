b2a83d4823560942923ea763d368ece305e507c4
Tek <tek@randhome.io>
2020-12-13 11:07:59 +0100

    Update github action

--- create-indicators-for-tinycheck.py
+++ create-indicators-for-tinycheck.py
@@ -1,46 +1,33 @@
-import json, csv, yaml
+import json, csv
 
-DOMAINS_FILE = 'network.csv'
-CERTIFICATES_FILE = 'certificates.yaml'
+NETWORK_FILE = 'network.csv'
 
 def load_stalkerware_domains():
-    try:
-        with open(DOMAINS_FILE, 'r') as domains_file:
-            csv_contents = csv.reader(domains_file, delimiter=',')
-            domains = [line[1] for line in csv_contents]
-            domains.pop(0)
-            return domains
-    except Exception:
-        print("Error loading {}".format(DOMAINS_FILE))
-        return False
+    with open(NETWORK_FILE, 'r') as f:
+        csv_contents = list(csv.reader(f, delimiter=','))
+        domains = [line[1] for line in csv_contents if line[0].strip() == "domain"]
+        ips = [line[1] for line in csv_contents if line[0].strip() == "ipv4"]
+        return domains, ips
 
-
-def load_stalkerware_certificates():
-    try:
-        with open(CERTIFICATES_FILE, 'r') as cert_file:
-            return yaml.load(cert_file, yaml.SafeLoader)
-    except Exception:
-        print("Error loading {}".format(DOMAINS_FILE))
-        return False
 
 iocs = {'iocs': []}
-domains = load_stalkerware_domains()
-for domain in domains:
-    iocs['iocs'].append({
-        'type': 'domain',
-        'tag': 'stalkerware',
-        'tlp': 'white',
-        'value': domain
-    })
-
-certificates = load_stalkerware_certificates()
-for c in certificates:
-    iocs['iocs'].append({
-        'type':'sha1cert',
-        'tag': 'stalkerware',
-        'tlp': 'white',
-        'value': c['certificate']
-    })
+domains, ips = load_stalkerware_domains()
+if domains:
+    for domain in domains:
+        iocs['iocs'].append({
+            'type': 'domain',
+            'tag': 'stalkerware',
+            'tlp': 'white',
+            'value': domain
+        })
+if ips:
+    for ip in ips:
+        iocs['iocs'].append({
+            'type': 'ip4addr',
+            'tag': 'stalkerware',
+            'tlp': 'white',
+            'value': ip
+        })
 
 with open('indicators-for-tinycheck.json', 'w') as f:
     f.write(json.dumps(iocs))
