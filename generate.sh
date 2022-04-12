296b20c5da8fe8f64c9ee3a4aa370977203f3570
U039b <git@0x39b.fr>
2022-04-12 17:39:21 +0200

    Generate Suricata rules

--- generate.sh
+++ generate.sh
@@ -3,3 +3,4 @@ python scripts/create-indicators-for-tinycheck.py
 python scripts/generate_hosts.py
 python scripts/generate_stix.py
 python scripts/make_misp_event.py
+python scripts/generate_suricata.py
